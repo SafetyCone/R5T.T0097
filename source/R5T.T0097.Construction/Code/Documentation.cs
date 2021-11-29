5206a05dc2c025ceea4b8b3d7ab9407563a75cfb
davidcoats <david@getsafetycone.com>
2021-11-28 23:47:44 -0800

    Initial commit of source-stub.

---
+++ source/R5T.T0097.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0097.Construction
+{
+    /// <summary>
+    /// Construction project for Project and IProject types..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0097/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0097
+{
+    /// <summary>
+    /// Project and IProject types.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
